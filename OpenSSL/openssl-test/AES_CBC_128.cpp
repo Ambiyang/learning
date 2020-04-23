b7feefc2d0a3202d5f110c321656eb0b8837e584
Yang Yatuo <toyangyatuo@163.com>
2020-04-23 12:52:07 +0900

    new

---
+++ OpenSSL/openssl-test/AES_CBC_128.cpp
@@ -0,0 +1,85 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openssl/aes.h>
+#define AES_BITS 128
+#define MSG_LEN 128
+
+int aes_encrypt(char* in, char* key, char* out,int len)
+{
+	if (!in || !key || !out) return 0;
+	unsigned char iv[AES_BLOCK_SIZE];
+	for (int i = 0; i < AES_BLOCK_SIZE; ++i)
+		iv[i] = 0;
+	AES_KEY aes;
+	if (AES_set_encrypt_key((unsigned char*)key, 128, &aes) < 0)
+	{
+		return 0;
+	}
+
+	AES_cbc_encrypt((unsigned char*)in, (unsigned char*)out, len, &aes, iv, AES_ENCRYPT);
+	return 1;
+}
+int aes_decrypt(char* in, char* key, char* out, int len)
+{
+	if (!in || !key || !out) return 0;
+	unsigned char iv[AES_BLOCK_SIZE];
+	for (int i = 0; i < AES_BLOCK_SIZE; ++i)
+		iv[i] = 0;//0;
+	AES_KEY aes;
+	if (AES_set_decrypt_key((unsigned char*)key, 128, &aes) < 0)
+	{
+		return 0;
+	}
+
+	AES_cbc_encrypt((unsigned char*)in, (unsigned char*)out, len, &aes, iv, AES_DECRYPT);
+	return 1;
+}
+
+int main()
+{
+	char sourceStringTemp[MSG_LEN];
+	char dstStringTemp[MSG_LEN];
+	memset((char*)sourceStringTemp, 0, MSG_LEN);
+	memset((char*)dstStringTemp, 0, MSG_LEN);
+	strcpy_s((char*)sourceStringTemp, 6,"HELLO");
+
+	char key[AES_BLOCK_SIZE];
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		key[i] = 49;
+	}
+
+	// Pad according to https://www.devglan.com/online-tools/aes-encryption-decryption specification
+	// without pad the result will be different from the above site's
+	for (i = strlen(sourceStringTemp); i < 16; i++)
+	{
+		sourceStringTemp[i] = 0x0b;
+	}
+
+	if (!aes_encrypt(sourceStringTemp, key, dstStringTemp,16))
+	{
+		printf("encrypt error\n");
+		return -1;
+	}
+	printf("enc %d:", strlen((char*)dstStringTemp));
+	for (i = 0;i<16;i += 1) {
+		printf("%x", (unsigned char)dstStringTemp[i]);
+	}
+	memset((char*)sourceStringTemp, 0, MSG_LEN);	
+
+	if (!aes_decrypt(dstStringTemp, key, sourceStringTemp, 16))
+	{
+		printf("decrypt error\n");
+		return -1;
+	}
+	printf("\n");
+	printf("dec %d:", strlen((char*)sourceStringTemp));
+	printf("%s\n", sourceStringTemp);
+	for (i = 0;sourceStringTemp[i];i += 1) {
+		printf("%02x", (unsigned char)sourceStringTemp[i]);
+	}
+	printf("\n");
+	return 0;
+}
---
+++ OpenSSL/openssl-test/ECC.cpp
@@ -0,0 +1,148 @@
+﻿#include <string.h>
+#include <stdio.h>
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/objects.h>
+#include <openssl/err.h>
+
+int main()
+{
+	EC_KEY* key1, * key2;
+	const EC_POINT* pubkey1, * pubkey2;
+	EC_GROUP* group1, * group2;
+	int ret, nid, size, i, sig_len;
+	unsigned char* signature, digest[20];
+	BIO* berr;
+	EC_builtin_curve* curves;
+	int crv_len;
+	char shareKey1[128], shareKey2[128];
+	int len1, len2;
+
+	key1 = EC_KEY_new();
+	if (key1 == NULL)
+	{
+		printf("EC_KEY_new err!\n");
+		return -1;
+	}
+	key2 = EC_KEY_new();
+	if (key2 == NULL)
+	{
+		printf("EC_KEY_new err!\n");
+		return -1;
+	}
+
+	/* get curves total number */
+	crv_len = EC_get_builtin_curves(NULL, 0);
+	curves = (EC_builtin_curve*)malloc(sizeof(EC_builtin_curve) * crv_len);
+	/* get curves list */
+	EC_get_builtin_curves(curves, crv_len);
+
+	/*
+	nid=curves[0].nid;会有错误，原因是密钥太短
+	*/
+
+	/* select a curve */
+	nid = curves[25].nid;
+	/* get key para group according to curve type */
+	group1 = EC_GROUP_new_by_curve_name(nid);
+	if (group1 == NULL)
+	{
+		printf("EC_GROUP_new_by_curve_name err!\n");
+		return -1;
+	}
+	group2 = EC_GROUP_new_by_curve_name(nid);
+	if (group1 == NULL)
+	{
+		printf("EC_GROUP_new_by_curve_name err!\n");
+		return -1;
+	}
+
+	/* set key para */
+	ret = EC_KEY_set_group(key1, group1);
+	if (ret != 1)
+	{
+		printf("EC_KEY_set_group err.\n");
+		return -1;
+	}
+	ret = EC_KEY_set_group(key2, group2);
+	if (ret != 1)
+	{
+		printf("EC_KEY_set_group err.\n");
+		return -1;
+	}
+
+	ret = EC_KEY_generate_key(key1);
+	if (ret != 1)
+	{
+		printf("EC_KEY_generate_key err.\n");
+		return -1;
+	}
+	ret = EC_KEY_generate_key(key2);
+	if (ret != 1)
+	{
+		printf("EC_KEY_generate_key err.\n");
+		return -1;
+	}
+
+	ret = EC_KEY_check_key(key1);
+	if (ret != 1)
+	{
+		printf("check key err.\n");
+		return -1;
+	}
+
+	size = ECDSA_size(key1);
+	printf("size %d \n", size);
+	for (i = 0;i < 20;i++)
+		memset(&digest[i], i + 1, 1);
+	signature = (unsigned char*)malloc(size);
+	ERR_load_crypto_strings();
+	berr = BIO_new(BIO_s_file());
+	BIO_set_fp(berr, stdout, BIO_NOCLOSE);
+
+	/* 签名数据，本例未做摘要，可将digest 中的数据看作是sha1 摘要结果 */
+	ret = ECDSA_sign(0, digest, 20, signature, (unsigned int*)&sig_len, key1);
+	if (ret != 1)
+	{
+		ERR_print_errors(berr);
+		printf("sign err!\n");
+		return -1;
+	}
+
+	ret = ECDSA_verify(0, digest, 20, signature, sig_len, key1);
+	if (ret != 1)
+	{
+		ERR_print_errors(berr);
+		printf("ECDSA_verify err!\n");
+		return -1;
+	}
+
+	/* Key Sharing */
+	pubkey2 = EC_KEY_get0_public_key(key2);
+	len1 = ECDH_compute_key(shareKey1, 128, pubkey2, key1, NULL);
+
+	pubkey1 = EC_KEY_get0_public_key(key1);
+	len2 = ECDH_compute_key(shareKey2, 128, pubkey1, key2, NULL);
+
+	if (len1 != len2)
+	{
+		printf("err\n");
+	}
+	else
+	{
+		ret = memcmp(shareKey1, shareKey2, len1);
+		if (ret == 0)
+			printf("Key sharing succeed\n");
+		else
+			printf("Key sharing failed\n");
+	}
+
+	printf("test ok!\n");
+	BIO_free(berr);
+	EC_KEY_free(key1);
+	EC_KEY_free(key2);
+	free(signature);
+	free(curves);
+	getchar();
+	return 0;
+}
